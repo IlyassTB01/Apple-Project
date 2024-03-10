a4bb527bd7276a3d6e6de22b5ed207d5be2ea5db
ILYASS TBER <160050768+IlyassTB01@users.noreply.github.com>
2024-03-10 22:06:06 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img =document.querySelector('.img')
+function phones(phone){
+    img.src = phone;
+}
+let container =document.querySelector('.container')
+
+function background(color){
+    container.style.background = color;
+}
